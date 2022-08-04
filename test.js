f37c98d7c353a447def0992a59e7175390e360c9
nadinechabbouh <110571126+nadinechabbouh@users.noreply.github.com>
2022-08-04 12:12:27 +0100

    Update test.js

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8081;
 
 // Listen on port 8081
 app.listen(PORT, () =>
-  console.log(Application is listening on port ${PORT}!)
+  console.log(`Application is listening on port ${PORT}!`)
 );
 //set index.html
 app.set('views','./Views');
